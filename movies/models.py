16a20df003b72298a0d7b5076098f75a09dd7cf0
PUCETEC <felipecepeda2006@hotmail.com>
2024-07-13 10:33:09 -0500

    Terminado examen2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie (models.Model): #clase nombre de la tabla
+    title = models.CharField(max_length= 60, null=False)
+    gender= models.CharField(max_length= 20, null=False)
+    director = models.CharField(max_length=40, null = False)
+    year = models.IntegerField(null = False)
+    synopsis = models.TextField(null = False)
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
+def index(request):
+    movies = Movie.objects.order_by('title')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
 
-# Create your views here.
+def movie(request, movie_id):
+    movie = Movie.objects.get(pk = movie_id) 
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
